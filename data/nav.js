0ed88fff60852b232b6d6e88ae60a7b69e215f67
Ramzy842 <ramzychahbani@gmail.com>
2024-02-28 06:03:19 +0100

    finished header

---
+++ data/nav.js
@@ -0,0 +1,76 @@
+const nav = [
+  {
+    title: "Game Pass",
+    links: [
+      {
+        title: "",
+        subLinks: [],
+      },
+    ],
+  },
+  {
+    title: "Games",
+    links: [
+      {
+        title: "",
+        subLinks: [],
+      },
+    ],
+  },
+  {
+    title: "Devices",
+    links: [
+      {
+        title: "",
+        subLinks: [],
+      },
+    ],
+  },
+  {
+    title: "Play",
+    links: [
+      {
+        title: "",
+        subLinks: [],
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        title: "",
+        subLinks: [],
+      },
+    ],
+  },
+  {
+    title: "Support",
+    links: [
+      {
+        title: "",
+        subLinks: [],
+      },
+    ],
+  },
+  {
+    title: "My Xbox",
+    links: [
+      {
+        title: "",
+        subLinks: [],
+      },
+    ],
+  },
+  {
+    title: "Developers",
+    links: [
+      {
+        title: "",
+        subLinks: [],
+      },
+    ],
+  },
+];
+
+export default nav;
---
+++ script.js
@@ -0,0 +1,21 @@
+import nav from "./data/nav.js";
+
+console.log(nav);
+
+const leftNavList = document.querySelector(".left_nav_list");
+
+const chevDown = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+  </svg>`
+
+
+nav.forEach((item) => {
+  const title = item.title;
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+  a.setAttribute("href", "#");
+  li.append(a);
+  a.innerHTML = `<span>${title}</span>${chevDown}`;
+  leftNavList.appendChild(li);
+});
